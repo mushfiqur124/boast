a7dd74ce7dd3d91caf6547a8590aec1bb1691a43
Mushfiqur Rahman <mushfiqurrahman124@gmail.com>
2024-03-08 22:39:23 -0500

    first commit

---
+++ BoastApp/BoastApp/BoastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoastAppApp.swift
+//  BoastApp
+//
+//  Created by Mushfiqur Rahman on 2024-03-08.
+//
+
+import SwiftUI
+
+@main
+struct BoastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
